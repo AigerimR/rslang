0806bd08be8a6fb0c97eeb8a671fab746c053cbf
Lazzat Mukhametalinova <2248lm@gmail.com>
2022-08-18 04:05:53 +0600

    feat: add Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import React, { FC } from 'react';
 import './styles/index.scss';
 import classes from './app.module.scss';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 const App: FC = () => {
   getWord('5e9f5ee35eb9e72bc21af4a0').then((word) => console.log(word));
   return (
     <div className={classes.wrapper} >
       <Header />
+      <Footer />
     </div >
   )
 };
