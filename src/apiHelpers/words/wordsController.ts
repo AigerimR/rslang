7ae15910aa2145aa40d91e90bc8be62ab31f7407
solomon khan <hansolomon168@gmail.com>
2022-08-17 11:21:53 +0600

    feat: add getWord request

--- src/apiHelpers/words/wordsController.ts
+++ src/apiHelpers/words/wordsController.ts
@@ -1,6 +1,6 @@
 import { EStatusCode } from './../../enums/serverStatusCode';
 import { TWord } from './../../@types/words';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
 const BASE_URL = 'https://team99-rslang-jsfe2022q1.herokuapp.com';
 
@@ -11,4 +11,13 @@ export const getWords = (group: number, page: number): Promise<TWord[]> => {
       response.status === EStatusCode.Ok ? response.data : Promise.reject(response),
     )
     .catch((repsonse) => console.log(repsonse));
+};
+
+export const getWord = (id: string): Promise<TWord> => {
+  return axios
+    .get(`${BASE_URL}/words/${id}`)
+    .then((response) =>
+      response.status === EStatusCode.Ok ? response.data : Promise.reject(response),
+    )
+    .catch((response: AxiosResponse) => console.error(response.statusText));
 };
