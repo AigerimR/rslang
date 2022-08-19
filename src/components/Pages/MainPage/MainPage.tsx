00c7de3d43e366de706c19088cc3a116c9e3a8ef
Lazzat Mukhametalinova <2248lm@gmail.com>
2022-08-20 02:39:51 +0600

    feat: create introduction page

--- src/components/Pages/MainPage/MainPage.tsx
+++ src/components/Pages/MainPage/MainPage.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import classes from './mainPage.module.scss';
+import Introduction from './Introduction/Introduction';
+import Description from './Description/Description';
+import WaveHeader from '../../Waves/WaveHeader';
+import WaveFooter from '../../Waves/WaveFooter';
 
-function MainPage() {
-  return <h2>THIS IS MAIN PAGE</h2>;
-}
+const MainPage = () => {
+  return (
+    <div className={classes.main_page_wrapper}>
+      <WaveHeader />
+      <main className={classes.main_page}>
+        <Introduction />
+        <Description />
+      </main>
+      <WaveFooter />
+    </div>
+  )
+};
 
 export default MainPage;
