fc16fcd8c8f2e462ab6bcab87f85c22d93880e50
Aigerim Raissova <raisovaaygerim@mail.ru>
2022-08-22 22:37:09 +0600

    feat: add pagination

--- src/components/Pages/Textbook/Textbook.tsx
+++ src/components/Pages/Textbook/Textbook.tsx
@@ -2,22 +2,28 @@ import React, { useEffect, useState } from 'react';
 import { TWord } from "../../../@types/words";
 import { getWords } from "../../../apiHelpers/words/wordsController";
 import Units from "./Units/Units";
+import Paginationmui from './Pagination/Paginationmui';
 
 const Textbook: React.FC = () => {
+  const [page, setPage] = useState<number>(0);
+  const updatePage = (x:number): number => { setPage(x); return x};
 
   const [unit, setUnit] = useState(0);
   const updateUnit = (u:number): number => { setUnit(u); return u};  
 
   let [data, setData] = useState<TWord[]>();
   const getData = async () => {
-    const res = await getWords(unit, 1);
+    const res = await getWords(unit, page);
     setData(res);
   }
 
+  useEffect(()=>{getData()}, [page, unit]);
+  
   return(
-    <div>
+    <>
       <Units updateUnit={updateUnit}/>
-    </div>
+      <Paginationmui updatePage={updatePage} />
+    </>
   );
 }
 
