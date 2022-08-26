564b8782c76dffd609e742f4e3c30c703d35725c
Lazzat Mukhametalinova <2248lm@gmail.com>
2022-08-26 18:22:22 +0600

    feat: fill checkAnswer function

--- src/components/Games/AudioGame/Audiocall.tsx
+++ src/components/Games/AudioGame/Audiocall.tsx
@@ -3,6 +3,7 @@ import classes from './audiocall.module.scss';
 import { TAudiocallWord } from '../../../@types/words';
 import { getAllAudiocallWords, getPageAudiocallWords } from '../../../apiHelpers/words/wordsController';
 import ISprintGameProps from '../../../interfaces/sprintGame';
+import { listenerCount } from 'process';
 
 const AudioGame: FC<ISprintGameProps> = ({
   difficultyLevel,
@@ -23,25 +24,51 @@ const AudioGame: FC<ISprintGameProps> = ({
       translate: [],
     },
   ]);
-  const [index, setIndex] = useState<number>(0);
+  let [index, setIndex] = useState<number>(0);
   const [loading, setLoading] = useState<boolean>(true);
   const [isVolumeUp, setVolumeUp] = useState<boolean>(true);
   const [isAnswerSelected, setAnswerSelected] = useState<boolean>(false);
   const [live, brokenLive]: string[] = ['favorite', 'heart_broken'];
   const hearts = new Array(5).fill(live);
+  const correctList: string[] = [];
+  const wrongList: string[] = [];
 
   useEffect(() => {
     const words: Promise<TAudiocallWord[] | void> = page
       ? getPageAudiocallWords(parseFloat(difficultyLevel), page)
       : getAllAudiocallWords(parseFloat(difficultyLevel));
-    words.then((words) => setWords(words)).finally(() => setLoading(false));
+    words.then((words) => setWords(words)).finally(() => setLoading(false))
   }, []);
 
   const audio = new Audio(wordsList[index].audio);
   const [w0, w1, w2, w3, w4]: string[] = wordsList[index].translate;
 
-  const checkAnswer = (n: number) => {
-    console.log([w0, w1, w2, w3, w4][n] === wordsList[index].rightTranslate);
+  const checkAnswer = (answer: string, n: number, word: string) => {
+    if (answer === word) {
+      handleScore(score + 10);
+      handleRightAnswer();
+      correctList.push(answer);
+    } else {
+      handleScore(score);
+      wrongList.push(answer);
+    }
+    setAnswerSelected(true);
+  }
+
+  console.log(correctList);
+  console.log(wrongList);
+  const handleUserAnswer = (answer: string, n: number) => {
+    handleAnswer();
+    checkAnswer(answer, n, wordsList[index].rightTranslate);
+  }
+
+  const nextPage = (status: boolean) => {
+    if (status) {
+      setIndex(index + 1);
+      setAnswerSelected(false);
+      const audio = new Audio(wordsList[index + 1].audio);
+      audio.play();
+    }
   }
 
   if (loading) return <p>Loading...</p>;
@@ -69,14 +96,13 @@ const AudioGame: FC<ISprintGameProps> = ({
           </div>
         </div>
         <ol className={classes.audioList}>
-          <li className={classes.audioList_item} onClick={() => checkAnswer(0)}>{w0}</li>
-          <li className={classes.audioList_item} onClick={() => checkAnswer(1)}>{w1}</li>
-          <li className={classes.audioList_item} onClick={() => checkAnswer(2)}>{w2}</li>
-          <li className={classes.audioList_item} onClick={() => checkAnswer(3)}>{w3}</li>
-          <li className={classes.audioList_item} onClick={() => checkAnswer(4)}>{w4}</li>
+          <li className={`${classes.audioList_item} item0`} onClick={() => { handleUserAnswer(w0, 0) }}>{w0}</li>
+          <li className={`${classes.audioList_item} item1`} onClick={() => { handleUserAnswer(w1, 1) }}>{w1}</li>
+          <li className={`${classes.audioList_item} item2`} onClick={() => { handleUserAnswer(w2, 2) }}>{w2}</li>
+          <li className={`${classes.audioList_item} item3`} onClick={() => { handleUserAnswer(w3, 3) }}>{w3}</li>
+          <li className={`${classes.audioList_item} item4`} onClick={() => { handleUserAnswer(w4, 4) }}>{w4}</li>
         </ol>
-        <button className={`${classes.audiocallBtn} ${classes.missWordBtn}`}> Не знаю</button>
-        <button className={`${'material-icons'} ${classes.audiocallBtn} ${classes.nextWordBtn}`} style={{ display: 'none' }}> east</button>
+        <button className={classes.audiocallBtn} onClick={() => { nextPage(true) }}> {isAnswerSelected ? 'Следующее' : 'Не знаю'}</button>
       </div>
     </div>
   );
