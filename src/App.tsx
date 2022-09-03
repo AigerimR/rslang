60b9d7c750f662d3bba6779dd0c79e795de2357f
solomon <hansolomon168@gmail.com>
2022-09-03 19:38:04 +0600

    refactor: removed eslint warnings from textbook component

--- src/App.tsx
+++ src/App.tsx
@@ -16,54 +16,55 @@ import GamePage from './components/Pages/GamePage/GamePage';
 import { LearnedWordsProvider } from './components/Context/LearnedWordsContext';
 
 const App: FC = () => {
-  // getWord('5e9f5ee35eb9e72bc21af4a0').then((word) => console.log(word));
-  const [userLogged, setUserLogged] = useState<boolean>((localStorage.getItem('userId') === null) ? false : true);
-  
+  const [userLogged, setUserLogged] = useState<boolean>(
+    localStorage.getItem('userId') === null ? false : true,
+  );
+
   return (
-    <UserContext.Provider value={{userLogged, setUserLogged}}>
-       <ComplexWordsProvider>
+    <UserContext.Provider value={{ userLogged, setUserLogged }}>
+      <ComplexWordsProvider>
         <LearnedWordsProvider>
-          <div className={classes.wrapper} >
+          <div className={classes.wrapper}>
             <Router>
               <Header />
-                <Routes>
-                  <Route path='/' element={<MainPage />} />
-                  <Route path='/textbook' element={<Textbook />} />
-                  <Route
-                    path='/audio-game'
-                    element={
-                      <GamePage
-                        title='Аудиовызов'
-                        gameDescription='Тренировка улучшает восприятие на слух'
-                        ruleDescription='В этой игре нужно выбрать перевод услышанного слова.'
-                        keysDescription='Для управления игрой с клавиатуры используй следующие клавиши: пробел - повторить аудио, цифры 1, 2, 3, 4, 5 - для выбора ответа, Enter - не знаю ответ или перейти к следующему слову. Либо можешь просто кликать мышью.'
-                        game='audiocall'
-                      />
-                    }
-                  />
-                  <Route
-                    path='/sprint-game'
-                    element={
-                      <GamePage
-                        title='Спринт'
-                        gameDescription='Спринт - это игра на скорость'
-                        ruleDescription='Даны слово на английском и его перевод, отметь правильно ли указан перевод для слова и набери как можно больше баллов.'
-                        game='sprint'
-                      />
-                    }
-                  />
-                  <Route path='/complex-words' element={<ComplexWords />} />
-                  <Route path='/dictionary' element={<Dictionary />} />
-                  <Route path='/statistics' element={<StatisticsPage />} />
-                  <Route path='/authorization' element={<Authorization />} />
-                </Routes>
+              <Routes>
+                <Route path='/' element={<MainPage />} />
+                <Route path='/textbook' element={<Textbook />} />
+                <Route
+                  path='/audio-game'
+                  element={
+                    <GamePage
+                      title='Аудиовызов'
+                      gameDescription='Тренировка улучшает восприятие на слух'
+                      ruleDescription='В этой игре нужно выбрать перевод услышанного слова.'
+                      keysDescription='Для управления игрой с клавиатуры используй следующие клавиши: пробел - повторить аудио, цифры 1, 2, 3, 4, 5 - для выбора ответа, Enter - не знаю ответ или перейти к следующему слову. Либо можешь просто кликать мышью.'
+                      game='audiocall'
+                    />
+                  }
+                />
+                <Route
+                  path='/sprint-game'
+                  element={
+                    <GamePage
+                      title='Спринт'
+                      gameDescription='Спринт - это игра на скорость'
+                      ruleDescription='Даны слово на английском и его перевод, отметь правильно ли указан перевод для слова и набери как можно больше баллов.'
+                      game='sprint'
+                    />
+                  }
+                />
+                <Route path='/complex-words' element={<ComplexWords />} />
+                <Route path='/dictionary' element={<Dictionary />} />
+                <Route path='/statistics' element={<StatisticsPage />} />
+                <Route path='/authorization' element={<Authorization />} />
+              </Routes>
               <Footer />
             </Router>
-          </div >
+          </div>
         </LearnedWordsProvider>
-       </ComplexWordsProvider>
+      </ComplexWordsProvider>
     </UserContext.Provider>
-  )
+  );
 };
 
 export default App;
--- src/components/Context/ComplexWordsContext.tsx
+++ src/components/Context/ComplexWordsContext.tsx
@@ -1,70 +1,72 @@
-import { createUserWord, deleteUserWord, getUserComplexWords } from '../../apiHelpers/users/usersController';
+import {
+  createUserWord,
+  deleteUserWord,
+  getUserComplexWords,
+} from '../../apiHelpers/users/usersController';
 import React, { createContext, useContext, useEffect, useState } from 'react';
 import { TWord } from '../../@types/words';
 import UserContext from './UserContext';
 
-export const ComplexWordsContext = createContext(
-  {
-    complexWords: [{ 
-    "id": "string",
-    "group": 0,
-    "page": 0,
-    "word": "string",
-    "image": "string",
-    "audio": "string",
-    "audioMeaning": "string",
-    "audioExample": "string",
-    "textMeaning": "string",
-    "textExample": "string",
-    "transcription": "string",
-    "wordTranslate": "string",
-    "textMeaningTranslate": "string",
-    "textExampleTranslate": "string"}],
-    getComplexWords: ()=>{},
-    addComplexWord: (wordId)=>{},
-    deleteComplexWord: (wordId)=>{},
+export const ComplexWordsContext = createContext({
+  complexWords: [
+    {
+      id: 'string',
+      group: 0,
+      page: 0,
+      word: 'string',
+      image: 'string',
+      audio: 'string',
+      audioMeaning: 'string',
+      audioExample: 'string',
+      textMeaning: 'string',
+      textExample: 'string',
+      transcription: 'string',
+      wordTranslate: 'string',
+      textMeaningTranslate: 'string',
+      textExampleTranslate: 'string',
+    },
+  ],
 });
 
-function ComplexWordsProvider (props) {
-  
-  const userId = localStorage.getItem("userId");
-  const token = localStorage.getItem("token");
+function ComplexWordsProvider(props) {
+  const userId = localStorage.getItem('userId');
+  const token = localStorage.getItem('token');
 
-  const { userLogged, setUserLogged } = useContext(UserContext);
+  const { userLogged } = useContext(UserContext);
 
   const [complexWords, setComplexWords] = useState<TWord[]>([]);
 
-  //to set initial values
   const getComplexWords = async (): Promise<void> => {
     const res = await getUserComplexWords(userId, token);
     setComplexWords(res);
-  }
-  useEffect(()=>{userLogged ? getComplexWords() : setComplexWords([])}, [userLogged]);
+  };
+  useEffect(() => {
+    userLogged ? getComplexWords() : setComplexWords([]);
+  }, [userLogged]);
 
   const addComplexWord = async (wordId: string) => {
     await createUserWord({
       userId: userId,
       wordId: wordId,
-      word: { 'difficulty': 'hard', 'optional': {} },
-      token: token
+      word: { difficulty: 'hard', optional: {} },
+      token: token,
     });
-    getComplexWords();  
-  }
+    getComplexWords();
+  };
 
   const deleteComplexWord = async (wordId: string) => {
     await deleteUserWord({
       userId: userId,
       wordId: wordId,
-      word: { 'difficulty': 'hard', 'optional': {} },
-      token: token
+      word: { difficulty: 'hard', optional: {} },
+      token: token,
     });
-    getComplexWords();  
-  }
+    getComplexWords();
+  };
 
   const ComplexWordsData = { complexWords, addComplexWord, deleteComplexWord };
   return <ComplexWordsContext.Provider value={ComplexWordsData} {...props} />;
 }
-
 
 function useComplexWordsContext() {
   return useContext(ComplexWordsContext);
