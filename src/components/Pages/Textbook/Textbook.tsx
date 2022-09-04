fc6e8000f18b6629fc43c52062803b06879eb7a7
Lazzat Mukhametalinova <2248lm@gmail.com>
2022-09-04 11:24:31 +0600

    refactor: correct card container style

--- src/components/Pages/Textbook/Textbook.tsx
+++ src/components/Pages/Textbook/Textbook.tsx
@@ -8,46 +8,50 @@ import classes from './Textbook.module.scss'
 import { Link } from 'react-router-dom';
 import starIcon from '../../../assets/svg/star.svg';
 import UserContext from '../../Context/UserContext';
+import Footer from '../../Footer/Footer';
 
 const Textbook: React.FC = () => {
 
   const [loading, setLoading] = useState<boolean>(true);
 
   const [page, setPage] = useState<number>(localStorage.getItem('page') === null ? 0 : +localStorage.getItem('page')!);
-  const updatePage = (page:number): void => { setPage(page); localStorage.setItem('page', `${page}`)};
+  const updatePage = (page: number): void => { setPage(page); localStorage.setItem('page', `${page}`) };
 
   const [unit, setUnit] = useState(localStorage.getItem('unit') === null ? 0 : +localStorage.getItem('unit')!);
-  const [unitColor, setUnitColor] = useState( localStorage.getItem('unitColor') === null ? '#bbd66c' :  localStorage.getItem('unitColor')!);
-  const updateUnit = (unit:number, unitColor: string): void => { setUnit(unit); setUnitColor(unitColor); localStorage.setItem('unitColor', `${unitColor}`), localStorage.setItem('unit', `${unit}`);  };  
+  const [unitColor, setUnitColor] = useState(localStorage.getItem('unitColor') === null ? '#bbd66c' : localStorage.getItem('unitColor')!);
+  const updateUnit = (unit: number, unitColor: string): void => { setUnit(unit); setUnitColor(unitColor); localStorage.setItem('unitColor', `${unitColor}`), localStorage.setItem('unit', `${unit}`); };
 
   const [data, setData] = useState<TWord[]>();
   const getData = async () => {
-    const res = await getWords(unit, page).finally(()=>setLoading(false));
+    const res = await getWords(unit, page).finally(() => setLoading(false));
     setData(res);
   }
 
-  useEffect(()=>{getData()}, [page, unit]);
+  useEffect(() => { getData() }, [page, unit]);
 
   const { userLogged, setUserLogged } = useContext(UserContext);
-  if(loading) return <h2 className={classes.main}>Loading...</h2>
-  return(
+  if (loading) return <h2 className={classes.main}>Loading...</h2>
+  return (
     <div className={classes.textbook}>
-      <div className={classes.textbook_header}>
-        <div></div>
-        <div>
-          <Units updateUnit={updateUnit}/>
-          <Paginationmui updatePage={updatePage} />
-        </div>
-        <Link to="/dictionary" className={userLogged ? classes.item_show : classes.item_none}>
-          <button className={classes.game_words}>
-            Словарь
-            <svg className={classes.btn_icon}>
+      <div className={classes.container}>
+        <div className={classes.textbook_header}>
+          <div></div>
+          <div>
+            <Units updateUnit={updateUnit} />
+            <Paginationmui updatePage={updatePage} />
+          </div>
+          <Link to="/dictionary" className={userLogged ? classes.item_show : classes.item_none}>
+            <button className={classes.game_words}>
+              Словарь
+              <svg className={classes.btn_icon}>
                 <use href={`${starIcon}#star`} />
-            </svg>
-          </button>
-        </Link>
+              </svg>
+            </button>
+          </Link>
+        </div>
+        <CardsContainer data={data!} unitColor={unitColor} inTextbook={true} />
       </div>
-      <CardsContainer data={data!} unitColor = {unitColor} inTextbook = {true}/>
+      <Footer />
     </div>
   );
 }
