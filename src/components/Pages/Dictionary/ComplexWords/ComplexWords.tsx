840b0c04b4a6a4bec8ecb42d2f3658775bf0dc5e
Lazzat Mukhametalinova <2248lm@gmail.com>
2022-09-04 15:08:31 +0600

    refactor: correct complex words style

--- src/components/Pages/Dictionary/ComplexWords/ComplexWords.tsx
+++ src/components/Pages/Dictionary/ComplexWords/ComplexWords.tsx
@@ -4,33 +4,39 @@ import Units from '../../Textbook/Units/Units';
 import { TWord, TWordUser } from '../../../../@types/words';
 import CardsContainer from '../../../CardsContainer/CardsContainer';
 import { useComplexWordsContext } from '../../../Context/ComplexWordsContext';
+import Footer from '../../../Footer/Footer';
 
 const ComplexWords: React.FC = () => {
   const [unit, setUnit] = useState(0);
   const [unitColor, setUnitColor] = useState('#bbd66c');
   const [data, setData] = useState<TWord[]>();
 
-  const updateUnit = (unit:number, unitColor: string): void => { setUnit(unit); setUnitColor(unitColor);};  
+  const updateUnit = (unit: number, unitColor: string): void => { setUnit(unit); setUnitColor(unitColor); };
 
-  const ComplexWordsContext  = useComplexWordsContext();
+  const ComplexWordsContext = useComplexWordsContext();
 
   const userId = localStorage.getItem("userId");
   const token = localStorage.getItem("token");
 
   const getData = () => {
-    const res = ComplexWordsContext.complexWords?.filter((word) => word.group === unit);    
+    const res = ComplexWordsContext.complexWords?.filter((word) => word.group === unit);
     setData(res);
   }
-  
-  useEffect(()=>{getData()}, [ComplexWordsContext.complexWords, unit]);
+
+  useEffect(() => { getData() }, [ComplexWordsContext.complexWords, unit]);
 
   return (
-    <div className={classes.complexW_main}>
-      <Units updateUnit={updateUnit}/>
-      <br/>
-      {data?.length === 0 ? <h2>No words yet</h2> : <CardsContainer data={data!} unitColor = {unitColor} inComplexComponent = {true}/>}
-      <br/>
-    </div>
+    <section className={classes.complexW_main}>
+      <div className={classes.container}>
+        <h2 className={classes.containerTitle}>Сложные слова</h2>
+        <Units updateUnit={updateUnit} />
+        <br />
+        <br />
+        {data?.length === 0 ? <p className={classes.complexW_info}>No words yet</p> : <CardsContainer data={data!} unitColor={unitColor} inComplexComponent={true} />}
+        <br />
+      </div>
+      <Footer />
+    </section>
   );
 }
 
